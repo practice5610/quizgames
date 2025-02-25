38511e6daefa1f72e2eb9889557a7126a5fb50a4
sami khan <109520665+practice5610@users.noreply.github.com>
2025-02-26 02:03:14 +0500

    live

--- okay.js
+++ okay.js
@@ -12,55 +12,53 @@
   gtag("config", "G-08WXH14EN4");
 })();
 
-window.googletag = window.googletag || {
-  cmd: [],
-};
+window.googletag = window.googletag || { cmd: [] };
+
 googletag.cmd.push(function () {
   googletag
     .defineSlot(
-      "/23282107112/f_1",
+      "/22847667724/ab_1",
       [[336, 280], [300, 250], "fluid"],
       "gpt-passback"
     )
     .addService(googletag.pubads());
   googletag.enableServices();
-  _();
 });
+
 googletag.cmd.push(function () {
   googletag.display("gpt-passback");
 });
-window.googletag = window.googletag || {
-  cmd: [],
-};
-if (Math.random() < 1.0) {
-  console.log("------>>>>>11", "");
+
+// Function to display rewarded ad on click
+function showRewardedAd() {
   let rewardedSlot;
   googletag.cmd.push(function () {
     rewardedSlot = googletag
       .defineOutOfPageSlot(
-        "/23282107112/f_2",
+        "/22847667724/ab_2",
         googletag.enums.OutOfPageFormat.REWARDED
       )
       .addService(googletag.pubads());
-
     googletag.enableServices();
-    _();
   });
 
   googletag.cmd.push(function () {
     googletag.display(rewardedSlot);
-
     googletag.pubads().addEventListener("rewardedSlotReady", function (evt) {
       evt.makeRewardedVisible();
     });
-
     googletag.pubads().addEventListener("rewardedSlotGranted", function () {
       console.log("Reward granted to user");
     });
-
     googletag.pubads().addEventListener("rewardedSlotClosed", function () {
       googletag.destroySlots([rewardedSlot]);
       console.log("Rewarded slot closed and destroyed");
     });
   });
 }
+
+// Attach event listeners to buttons
+window.onload = function () {
+  document.getElementById("above-18").addEventListener("click", showRewardedAd);
+  document.getElementById("below-18").addEventListener("click", showRewardedAd);
+};
