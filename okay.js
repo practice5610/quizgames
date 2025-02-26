9d24e3a6e98b7f2871d8ca81980413844fa967aa
sami khan <109520665+practice5610@users.noreply.github.com>
2025-02-26 20:49:39 +0500

    live

--- okay.js
+++ okay.js
@@ -18,7 +18,7 @@ window.googletag = window.googletag || {
 googletag.cmd.push(function () {
   googletag
     .defineSlot(
-      "/23282107112/f_1",
+      "/22870505798/jk_1",
       [[336, 280], [300, 250], "fluid"],
       "gpt-passback"
     )
@@ -38,7 +38,7 @@ if (Math.random() < 1.0) {
   googletag.cmd.push(function () {
     rewardedSlot = googletag
       .defineOutOfPageSlot(
-        "/23282107112/f_2",
+        "/22870505798/jk_2",
         googletag.enums.OutOfPageFormat.REWARDED
       )
       .addService(googletag.pubads());
